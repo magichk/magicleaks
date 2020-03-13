01918659336b296a5aeb000c1b90eacf188ad900
BinaryShadow94 <josemanu.infor@gmail.com>
2020-03-13 21:51:37 +0100

    Fix colors and other problems

--- magicleaks.py
+++ magicleaks.py
@@ -10,13 +10,13 @@ import hashlib  # To create the email hash for certain webs
 
 # Print banner
 
-print("\033[1;33;40m __  __    _    ____ ___ ____ _     _____    _    _  ______  ")
-print("\033[1;33;40m|  \/  |  / \  / ___|_ _/ ___| |   | ____|  / \  | |/ / ___| ")
-print("\033[1;33;40m| |\/| | / _ \| |  _ | | |   | |   |  _|   / _ \ | ' /\___ \ ")
-print("\033[1;33;40m| |  | |/ ___ \ |_| || | |___| |___| |___ / ___ \| . \ ___) |")
-print("\033[1;33;40m|_|  |_/_/   \_\____|___\____|_____|_____/_/   \_\_|\_\____/ ")
-print("\033[1;33;40m                                                             ")
-print("\033[1;37;40mBy Magichk and BinaryShadow")
+print("\033[1;33;40m __  __    _    ____ ___ ____ _     _____    _    _  ______  \33[00m")
+print("\033[1;33;40m|  \/  |  / \  / ___|_ _/ ___| |   | ____|  / \  | |/ / ___| \33[00m")
+print("\033[1;33;40m| |\/| | / _ \| |  _ | | |   | |   |  _|   / _ \ | ' /\___ \ \33[00m")
+print("\033[1;33;40m| |  | |/ ___ \ |_| || | |___| |___| |___ / ___ \| . \ ___) |\33[00m")
+print("\033[1;33;40m|_|  |_/_/   \_\____|___\____|_____|_____/_/   \_\_|\_\____/ \33[00m")
+print("\033[1;33;40m                                                             \33[00m")
+print("\033[1;37;40m--> By Magichk and BinaryShadow\33[00m\n")
 
 
 # Check Arguments
@@ -39,13 +39,13 @@ def checkArgs():
 
 # Help usage
 def helpUsage():
-	print("\033[1;33;40mMagicLeaks 0.1 Beta")
-	print("\033[1;33;40mUsage: magicleaks.py [Options] {target specification}")
-	print("\033[1;33;40mOptions Avilable")
-	print("\033[1;33;40m\t-e, --email         email address to search")
-	print("\033[1;33;40m\t-f, --file          File with email accounts to search leaks")
-	print("\033[1;33;40m\t-h, --help          Help")
-	print("\033[1;33;40m                        ")
+	print("\033[1;33mMagicLeaks 0.1 Beta")
+	print("\033[1;33mUsage: magicleaks.py [Options] {target specification}")
+	print("\033[1;33mOptions Avilable")
+	print("\033[1;33m\t-e, --email         email address to search")
+	print("\033[1;33m\t-f, --file          File with email accounts to search leaks")
+	print("\033[1;33m\t-h, --help          Help")
+	print("\033[1;33m                        ")
 
 
 # Check the email in Firefox Monitor
@@ -56,28 +56,32 @@ def check_email(email):
 		check_firefox_monitor(email)
 		check_pasterbinLeaks(email)
 	else:
-		print("\033[1;31;40mError: La direccion de correo no esta en un formato correcto")
+		print("\033[1;31mError: La direccion de correo no esta en un formato correcto")
 
 def parse_firefox_monitor(response):
 	start_breachName = response.text.find("breach-title")
+	leaks = False
 	while start_breachName != -1:
-		print("\033[1;31;40mLeak Detected!!!")
+		leaks = True
+		print("\033[1;31mLeak Detected!!!")
 		start_breachName = start_breachName + 14
 		end_breachName = response.text.find("</span>", start_breachName)
-		print("\033[1;31;40m\t--> " + response.text[start_breachName:end_breachName])
+		print("\033[1;31m\t--> " + response.text[start_breachName:end_breachName])
 		end_key = end_breachName
 		start_index = response.text.find("breach-key", end_key) + 12
 		while start_index > 12:
 			end_index = response.text.find("</span>", start_index)
 			start_key = response.text.find("breach-value", end_index) + 14
 			end_key = response.text.find("</span>", start_key)
-			print("\033[1;31;40m\t\t- " + response.text[start_index:end_index] + " " + response.text[start_key:end_key])
+			print("\033[1;31m\t\t- " + response.text[start_index:end_index] + " " + response.text[start_key:end_key])
 			start_index = response.text.find("breach-key", end_key) + 12
 		start_breachName = response.text.find("breach-title", end_breachName)
+	if not leaks:
+		print("\033[1;32mThis email account not appears on Firefox Monitor")
 
 def check_firefox_monitor(email):
-	print("\033[1;30;40mChecking email account " + email + " on Firefox Monitor...")
-	print("\033[1;30;40m-----------------------------------------------")
+	print("\033[1;33mChecking email account " + email + " on Firefox Monitor...")
+	print("\033[1;33m-----------------------------------------------")
 	# Extract valid csrf token from request.
 	url_form = 'https://monitor.firefox.com'
 	headers = {
@@ -105,33 +109,35 @@ def check_firefox_monitor(email):
 			response = client.post(url, params)
 			client.close()
 			parse_firefox_monitor(response)
-	print("\033[1;30;40m--------------------------------------------------------")
-	print("\033[1;30;40m--------------------------------------------------------")
+	else:
+		print("\033[1;31mError: No ha sido posible conectar con Firefox Monitor (existe un limite de peticiones por hora)")
+	print("\033[1;37m--------------------------------------------------------")
+	print("\033[1;37m--------------------------------------------------------")
 
 def check_pasterbinLeaks(email):
-	print("\033[1;30;40mChecking email account " + email + " on pastebin leaks...")
-	print("\033[1;30;40m-----------------------------------------------")
+	print("\033[1;33mChecking email account " + email + " on pastebin leaks...")
+	print("\033[1;33m-----------------------------------------------")
 	r = requests.get("https://psbdmp.ws/api/search/" + email)
 	resp_json = json.loads(r.text)
 
 	total = resp_json["count"]
 	if (total > 0):
-		print("\033[1;31;40mThis email account " + email + " appears on pastebin in " + str(
+		print("\033[1;31mThis email account " + email + " appears on pastebin in " + str(
 			total) + " results listed bellow:")
-		print("\033[1;31;40m ")
+		print("\033[1;31m ")
 		cont = 0
 		while (cont <= (total - 1)):
 			link = "https://pastebin.com/" + str(resp_json["data"][cont]["id"])
 			print(link)
 			cont = cont + 1
 	else:
-		print("\033[1;32;40mThis email account not appears on pastebin leaks")
-	print("\033[1;30;40m--------------------------------------------------------")
-	print("\033[1;30;40m--------------------------------------------------------")
+		print("\033[1;32mThis email account not appears on pastebin leaks")
+	print("\033[1;37m--------------------------------------------------------")
+	print("\033[1;37m--------------------------------------------------------")
 
 def emailreputation(email):
-	print("\033[1;30;40mChecking emailrep.io for " + email + " account ")
-	print("\033[1;30;40m-----------------------------------------------")
+	print("\033[1;33mChecking emailrep.io for " + email + " account ")
+	print("\033[1;33m-----------------------------------------------")
 	response = requests.get('https://emailrep.io/' + email)
 	emailreputation = json.loads(response.text)
 	try:
@@ -140,18 +146,18 @@ def emailreputation(email):
 		data_breach = emailreputation["details"]["data_breach"]
 		last_seen = emailreputation["details"]["last_seen"]
 		print(" ")
-		print("\033[1;37;40mEmail reputation")
+		print("\033[1;37mEmail reputation")
 		print("-----------------------------")
 		if (credentials_leaked == True or data_breach == True):
-			print("\033[1;31;40mThis email account has " + reputation + " reputation\nCredentials leaked? " + str(
+			print("\033[1;31mThis email account has " + reputation + " reputation\nCredentials leaked? " + str(
 				credentials_leaked) + "\nHas data breach? " + str(data_breach) + "\nLast seen: " + str(last_seen))
 		else:
-			print("\033[1;32;40mThis email account has " + reputation + " reputation\nCredentials leaked? " + str(
+			print("\033[1;32mThis email account has " + reputation + " reputation\nCredentials leaked? " + str(
 				credentials_leaked) + "\nHas data breach? " + str(data_breach))
 	except:
-		print("\033[1;31;40mError: " + emailreputation["reason"])
-	print("\033[1;30;40m--------------------------------------------------------")
-	print("\033[1;30;40m--------------------------------------------------------")
+		print("\033[1;31mError: " + emailreputation["reason"])
+	print("\033[1;37m--------------------------------------------------------")
+	print("\033[1;37m--------------------------------------------------------")
 
 
 # Main function
@@ -164,20 +170,22 @@ def main():
 			pattern = "(^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$)"
 			result = re.match(pattern, sys.argv[2])
 			if (result):
-				print("\033[1;37;40mChecking this email account...")
-				print("\033[1;37;40m------------------------------")
 				email = sys.argv[2]
+				print("\033[1;37mChecking email account " + email + " ...")
+				print("\033[1;37m------------------------------")
 				check_email(email)
 				emailreputation(email)
 		except:
-			print("\033[1;31;40mEl formato del email no es correcto o ha habido un problema durante la peticion!")
+			print("\033[1;31mEl formato del email no es correcto o ha habido un problema durante la peticion!")
 	elif (check == 2):
 		try:
-			print("\033[1;37;40mReading file with email accounts...")
-			print("\033[1;37;40m-----------------------------------")
+			print("\033[1;37mReading file with email accounts...")
+			print("\033[1;37m-----------------------------------\n")
 			with open(sys.argv[2]) as myfile:
 				lines = myfile.readlines()
 			for email in lines:
+				print("\033[1;37mChecking email account " + email + " ...")
+				print("\033[1;37m------------------------------")
 				email = email[0:len(email) - 1]
 				check_email(email)
 				try:
@@ -185,9 +193,9 @@ def main():
 				except:
 					print("No hay informacion disponible de este email")
 		except IOError:
-			print("\033[1;31;40mEl formato del fichero no es correcto o bien el fichero no existe")
+			print("\033[1;31mEl formato del fichero no es correcto o bien el fichero no existe")
 	elif (check == 0):
-		print("\033[1;31;40mOpcion no valida")
+		print("\033[1;31mOpcion no valida")
 
 
 # Execute main function
